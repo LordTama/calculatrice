abc3619ed9f588616362a054a01e1df46ef3e6ef
Mickaël Moreira <mickael.moreira@gmail.com>
2018-06-06 23:50:42 +0200

    Initial Commit

---
+++ calculatrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculatrice
+//
+//  Created by Mickaël Moreira on 06/06/2018.
+//  Copyright © 2018 Mickaël Moreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
